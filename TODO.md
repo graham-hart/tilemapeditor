9190c48edd18bdbe175a3f853f969a755f2f8559
Graham Hart <graham@harts.net>
2021-09-05 01:38:32 -0700

    Todo list

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+- Display current layer, tool, and mouse coords
+- Fill tool
+- More sophisticated palette (tile variations)
+- Auto-tiling
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import math
 import sys
 from enum import Enum
@@ -6,7 +7,6 @@ import pygame as pg
 
 import utils
 from map import Map
-import argparse
 
 
 class DrawMode(Enum):
@@ -27,10 +27,11 @@ COLORS = {
 CONFIG = {
     "draw_mode": DrawMode.PEN,
     "selected_img": "",
-    "save_on_exit": True,
+    "save_on_exit": False,
     "remove_tiles_not_in_palette": True,
     "move_speed": 3,
     "scale": 16,
+    "curr_layer": 0,
 }
 
 CONTROLS = {
@@ -48,6 +49,7 @@ CONTROLS = {
 }
 SIZE = WIDTH, HEIGHT = 1600, 900
 SIDEBAR_SIZE = SIDEBAR_WIDTH, SIDEBAR_HEIGHT = WIDTH / 4, HEIGHT
+MAP_SIZE = MAP_WIDTH, MAP_HEIGHT = WIDTH - SIDEBAR_WIDTH, HEIGHT
 
 
 def exit(tilemap):
@@ -78,7 +80,7 @@ def swap_image(key, palette):
             CONFIG["selected_img"] = list(palette.keys())[num - 1]
 
 
-def key_down(key, tilemap, movement, palette):
+def key_down(key, tilemap, palette):
     if key in CONTROLS["exit"]:
         exit(tilemap)
     elif key in CONTROLS["save"]:
@@ -89,14 +91,6 @@ def key_down(key, tilemap, movement, palette):
         CONFIG["draw_mode"] = DrawMode.PEN
     elif key in CONTROLS["no_draw"]:
         CONFIG["draw_mode"] = DrawMode.NONE
-    elif key in CONTROLS["move_up"]:
-        movement[1] = CONFIG["move_speed"]
-    elif key in CONTROLS["move_down"]:
-        movement[1] = -CONFIG["move_speed"]
-    elif key in CONTROLS["move_left"]:
-        movement[0] = -CONFIG["move_speed"]
-    elif key in CONTROLS["move_right"]:
-        movement[0] = CONFIG["move_speed"]
     elif key in CONTROLS["zoom_in"] and CONFIG["scale"] <= 62:
         CONFIG["scale"] += 2
     elif key in CONTROLS["zoom_out"] and CONFIG["scale"] > 2:
@@ -105,14 +99,7 @@ def key_down(key, tilemap, movement, palette):
         swap_image(key, palette)
 
 
-def key_up(key, movement):
-    if key in CONTROLS["move_up"] or key in CONTROLS["move_down"]:
-        movement[1] = 0
-    elif key in CONTROLS["move_left"] or key in CONTROLS["move_right"]:
-        movement[0] = 0
-
-
-def mouse_down(btn, pos, palette, tilemap, translation):
+def mouse_down(btn, pos, palette):
     if btn == pg.BUTTON_LEFT:
         x, y = pos
         if x <= SIDEBAR_WIDTH:
@@ -120,29 +107,21 @@ def mouse_down(btn, pos, palette, tilemap, translation):
                 if img[1].x <= x <= img[1].x + img[1].width and img[1].y <= y <= img[1].y + img[1].height:
                     CONFIG["selected_img"] = key
                     break
-        elif CONFIG["draw_mode"] != DrawMode.NONE:
-            loc = world_mouse_pos(translation, *pos)
-            if CONFIG["draw_mode"] == DrawMode.PEN:
-                tilemap[loc] = CONFIG["selected_img"]
-            elif CONFIG["draw_mode"] == DrawMode.ERASE and loc in tilemap:
-                del tilemap[loc]
 
 
-def handle_event(evt, palette, tilemap, translation, movement):
+def handle_event(evt, palette, tilemap):
     if evt.type == pg.QUIT:
         exit(tilemap)
     elif evt.type == pg.KEYDOWN:
-        key_down(evt.key, tilemap, movement, palette)
-    elif evt.type == pg.KEYUP:
-        key_up(evt.key, movement)
+        key_down(evt.key, tilemap, palette)
     elif evt.type == pg.MOUSEBUTTONDOWN:
-        mouse_down(evt.button, evt.pos, palette, tilemap, translation)
+        mouse_down(evt.button, evt.pos, palette)
 
 
-def draw_map(tilemap, screen, palette, translation):
+def draw_map(tilemap, surface, palette, translation):
     for k, v in sorted(tilemap.items(), reverse=True):
         pos = utils.parse_map_key(k)
-        rect = pg.Rect(int(pos[0]) * CONFIG["scale"] + SIDEBAR_WIDTH, int(pos[1]) * CONFIG["scale"],
+        rect = pg.Rect(int(pos[0]) * CONFIG["scale"], int(pos[1]) * CONFIG["scale"],
                        CONFIG["scale"], CONFIG["scale"])
         img = palette[v][0]
         if CONFIG["scale"] != 1:
@@ -150,8 +129,8 @@ def draw_map(tilemap, screen, palette, translation):
         rect.move_ip(translation)
         rect.inflate(CONFIG["scale"], CONFIG["scale"])
         if v in palette.keys():
-            if SIDEBAR_WIDTH - rect.width <= rect.x <= WIDTH and -rect.height <= rect.y <= HEIGHT:
-                screen.blit(img, rect)
+            if -rect.width <= rect.x <= MAP_WIDTH and -rect.height <= rect.y <= MAP_HEIGHT:
+                surface.blit(img, rect)
         else:
             print(f"Image '{v}' not found in palette.")
             if CONFIG["remove_tiles_not_in_palette"]:
@@ -169,30 +148,86 @@ def draw_cursor(sc, x, y):
     pg.draw.line(sc, color, (x, y + length), (x, y + gap), width=width)
 
 
+def sanitize_map(tilemap, palette):
+    for k, v in sorted(tilemap.items(), reverse=True):
+        if v not in palette.keys():
+            pos = utils.parse_map_key(k)
+            if CONFIG["remove_tiles_not_in_palette"]:
+                del tilemap[pos]
+
+
+def key_pressed_in_list(lst):
+    k = pg.key.get_pressed()
+    for i in lst:
+        if k[i]:
+            return True
+    return False
+
+
+def move(translation):
+    if key_pressed_in_list(CONTROLS["move_up"]):
+        translation[1] += CONFIG["move_speed"]
+    if key_pressed_in_list(CONTROLS["move_down"]):
+        translation[1] -= CONFIG["move_speed"]
+    if key_pressed_in_list(CONTROLS["move_left"]):
+        translation[0] += CONFIG["move_speed"]
+    if key_pressed_in_list(CONTROLS["move_right"]):
+        translation[0] -= CONFIG["move_speed"]
+
+
 def main():
+    # ----------------------------------------------------------- Parse CLI args for file IO
     parser = argparse.ArgumentParser()
     parser.add_argument("output", type=str, help="Output file for the map")
     parser.add_argument("-i", type=str, help="Input file for the map (optional)", metavar="input")
     args = parser.parse_args()
+
+    # ----------------------------------------------------------- Basic Pygame Initialization
     pg.init()
     pg.mouse.set_visible(False)
-    sc = pg.display.set_mode(SIZE)
+    screen = pg.display.set_mode(SIZE)
     pg.display.set_caption("Tile Map Editor")
+
+    # ----------------------------------------------------------- Setup surfaces for rendering
+    sidebar_surf = pg.Surface(SIDEBAR_SIZE)
+    sidebar_surf_rect = sidebar_surf.get_rect()
+    map_surf = pg.Surface(MAP_SIZE)
+    map_surf_rect = map_surf.get_rect().move(SIDEBAR_WIDTH, 0)
+
+    # ----------------------------------------------------------- Palette setup
     PALETTE = {d[0]: (pg.transform.scale(d[1], (20, 20)), pg.Rect(WIDTH / 18, index * 24 + 32, 20, 20)) for index, d in
                enumerate(utils.load_image_dir("imgs").items())}
     CONFIG["selected_img"] = list(PALETTE.keys())[0]
-    MAP = Map(args.output, args.i)
+
+    # ----------------------------------------------------------- Map & Translation
+    TILEMAP = Map(args.output, args.i)
     translation = [0, 0]
-    movement = [0, 0]
     while True:
+        # ----------------------------------------------------------- Events
         for evt in pg.event.get():
-            handle_event(evt, PALETTE, MAP, translation, movement)
-        translation[0] -= movement[0]
-        translation[1] += movement[1]
-        sc.fill(COLORS["BLACK"])
-        draw_map(MAP, sc, PALETTE, translation)
-        draw_sidebar(PALETTE, sc)
-        draw_cursor(sc, *pg.mouse.get_pos())
+            handle_event(evt, PALETTE, TILEMAP)
+
+        # ----------------------------------------------------------- Paint on map
+        if pg.mouse.get_pressed(3)[pg.BUTTON_LEFT - 1]:
+            if CONFIG["draw_mode"] != DrawMode.NONE:
+                pos = pg.mouse.get_pos()
+                if pos[0] > SIDEBAR_WIDTH:
+                    loc = world_mouse_pos(translation, *pos)
+                    if CONFIG["draw_mode"] == DrawMode.PEN:
+                        TILEMAP[loc] = CONFIG["selected_img"]
+                    elif CONFIG["draw_mode"] == DrawMode.ERASE and loc in TILEMAP:
+                        del TILEMAP[loc]
+
+        move(translation)  # Change translation based on input (if any)
+
+        # ----------------------------------------------------------- Draw onto screen
+        map_surf.fill(COLORS["BLACK"])
+
+        draw_map(TILEMAP, map_surf, PALETTE, translation)
+        draw_sidebar(PALETTE, sidebar_surf)
+        screen.blit(map_surf, map_surf_rect)
+        screen.blit(sidebar_surf, sidebar_surf_rect)
+        draw_cursor(screen, *pg.mouse.get_pos())
         pg.display.flip()
 
 
